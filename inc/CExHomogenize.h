224acefe7c9997997cbaf986b9ed5c665ae1bf09
khalid.n.ismail <khalid.n.ismail@googlemail.com>
2015-03-20 16:01:18 +0000

    Updating Framework

---
+++ inc/CExHomogenize.h
@@ -0,0 +1,21 @@
+/*
+ * CExHomogenize.h
+ *
+ *  Created on: 20 Mar 2015
+ *      Author: knai20-admin
+ */
+
+#ifndef CEXHOMOGENIZE_H_
+#define CEXHOMOGENIZE_H_
+
+class CExHomogenize {
+public:
+	CExHomogenize();
+	virtual ~CExHomogenize();
+
+public:
+
+	void Execute();
+};
+
+#endif /* CEXHOMOGENIZE_H_ */
