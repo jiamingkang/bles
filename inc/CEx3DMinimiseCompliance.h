939394f241f8e01a296d38ff52feac246793cbc9
khalid.n.ismail <khalid.n.ismail@googlemail.com>
2015-03-20 16:01:49 +0000

    Updating Framework

---
+++ inc/CEx3DMinimiseCompliance.h
@@ -0,0 +1,22 @@
+/*
+ * CEx3DMinimiseCompliance.h
+ *
+ *  Created on: 20 Mar 2015
+ *      Author: knai20-admin
+ */
+
+#ifndef CEX3DMINIMISECOMPLIANCE_H_
+#define CEX3DMINIMISECOMPLIANCE_H_
+
+class CEx3DMinimiseCompliance {
+public:
+	CEx3DMinimiseCompliance();
+	virtual ~CEx3DMinimiseCompliance();
+
+public:
+
+	void Execute();
+
+};
+
+#endif /* CEX3DMINIMISECOMPLIANCE_H_ */
