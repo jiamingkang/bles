e9980549b3b49ec118b08b903f93982a727b8af3
khalid.n.ismail <khalid.n.ismail@googlemail.com>
2015-03-20 16:00:23 +0000

    Updating Framework

---
+++ inc/CExStress.h
@@ -0,0 +1,21 @@
+/*
+ * CExStress.h
+ *
+ *  Created on: 20 Mar 2015
+ *      Author: knai20-admin
+ */
+
+#ifndef CEXSTRESS_H_
+#define CEXSTRESS_H_
+
+class CExStress {
+public:
+	CExStress();
+	virtual ~CExStress();
+
+public:
+
+	void Execute();
+};
+
+#endif /* CEXSTRESS_H_ */
